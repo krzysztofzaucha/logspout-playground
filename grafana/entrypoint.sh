0778b3c7d6766f6a73f830ed6ddaf4ce3e2bcaf4
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2022-05-11 01:40:38 +0100

    main(multiple): fix grafana

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-until mysql -h "${HOST}" -u"${USERNAME}" -p"${PASSWORD}" -P"${PORT}" "${NAME}"; do
+until mysql -h "${MYSQL_HOST}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" -P"${MYSQL_PORT}" "${MYSQL_NAME}"; do
   >&2 echo "waiting for MariaDB to be ready..."
   sleep 1
 done
